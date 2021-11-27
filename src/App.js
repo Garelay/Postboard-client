e6e0cb4c19901e8e559f813144671cb6664ab559
Konstantin Mochalov <constantin.mochalov@gmail.com>
2021-11-27 17:45:37 +0300

    added display all posts and add new post functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import {getPosts} from "./actions/posts"
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import styles from "./App.module.css"
 
 function App() {
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
   return (
     <div className="App">
-          Learn React
+      <div className={styles.container}>
+        <Posts setCurrentId={setCurrentId}/>
+        <Form setCurrentId={setCurrentId}/>
+      </div> 
     </div>
+    
+
   );
 }
 
